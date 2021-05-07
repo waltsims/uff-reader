1eb37a72f197950456f121c615691c0973d70ec2
Walter Simson <walter.simson@tum.de>
2021-05-07 13:36:03 +0200

    Reformat and fix typos

--- tests/test_aperture.py
+++ tests/test_aperture.py
@@ -2,8 +2,7 @@ from uff.aperture import Aperture
 from uff.position import Position
 
 
-def test_instatiation():
-    p1=Position(0,0,0)
+def test_instantiation():
+    p1 = Position(0, 0, 0)
     Aperture(origin=p1, window='Hamming', f_number=1, fixed_size=1)
     pass
-
