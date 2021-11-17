44741df17e592304fdba68129618f287cadb7916
Walter Simson <walter.simson@tum.de>
2021-11-17 14:21:32 +0100

    Update and optimize downloader
    - Add fixture directory for data
    - Only download missing files

--- tests/test_uff.py
+++ tests/test_uff.py
@@ -4,9 +4,13 @@ from pathlib import Path
 from uff.uff import UFF
 from uff.utils import verify_correctness, is_version_compatible, load_uff_dict, download_test_data
 
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'data',
+)
+
 
 def test_uff_save_load():
-    ref_dir = './data'
     ref_files = [
         'fieldII_converging_wave_mlt_sector.uff',
         'fieldII_converging_wave_grid.uff',
@@ -16,15 +20,16 @@ def test_uff_save_load():
     ]
 
     # check all files exist in data/
-    if not all([os.path.isfile(Path(ref_dir) / file) for file in ref_files]):
+    missing_files = [file for file in ref_files if not os.path.isfile(Path(FIXTURE_DIR) / file)]
+    if missing_files:
         print("Downloading test files...")
         # if they do not download them with utils.
         base_url = 'http://ustb.no/datasets/uff/'
-        urls = [base_url + file for file in ref_files]
-        download_test_data(rel_path=ref_dir, file_urls=urls)
+        urls = [base_url + file for file in missing_files]
+        download_test_data(rel_path=FIXTURE_DIR, file_urls=urls)
 
     for ref_file in ref_files:
-        ref_uff_path = os.path.join(ref_dir, ref_file)
+        ref_uff_path = os.path.join(FIXTURE_DIR, ref_file)
         uff_dict = load_uff_dict(ref_uff_path)
 
         version = uff_dict.pop('version')
