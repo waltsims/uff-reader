2a39e1bee56290a8f6de75300b570bfb29309c0a
Walter Simson <walter.simson@tum.de>
2022-01-29 14:32:43 +0100

    Add tests for issue #19
    begin to add solution for "Additional" and "non-standard-conforming" object properties.
    
    closes #17

--- src/uff/probe.py
+++ src/uff/probe.py
@@ -40,17 +40,44 @@ class Probe(Serializable):
     # def deserialize(cls, data: dict):
     #     pass
 
-    # >> TODO: These parameters are not defined in the standard
-    number_elements: int
-    pitch: float
-    element_height: float
-    element_width: float
-    ##  <<
     transform: Transform
-    # TODO for conformity call `elements`
     element: List[Element]
     element_impulse_response: List[ImpulseResponse] = None
     focal_length: float = None
-    # >> TODO: These parameters are not defined in the standard
     element_geometry: List[ElementGeometry] = None
+    # # >> TODO: These parameters are not defined in the standard
+    number_elements: int = 0
+    pitch: float = 0
+    element_height: float = 0
+    element_width: float = 0
     ##  <<
+    ## TODO: add element list factory to generate element list from these parameters,
+    # and getters to generate properties from these properties.
+    # OPEN QUESTION: what to do when file contains extra properties that do not conform to standard?
+    # TODO idea for getters
+    # @property
+    # def number_elements(self):
+    #     return len(self.element)
+    # @property
+    # def pitch(self):
+    #     # check if all element spacings are the same
+    #     # return pitch
+    #     # else:
+    #     # catch and warn of non-standard spacing
+    #     return len(self.element)
+    #
+    # @property
+    # def element_width(self):
+    #     # check for conformal element width
+    #     # return element_width
+    #     # else:
+    #     # catch and warn of non-conformal element width
+    #     pass
+    #
+    # @property
+    # def element_height(self):
+    #     # check for conformal element height
+    #     # return element_height
+    #     # else:
+    #     # catch and warn of non-conformal element height
+    #     pass
