2e4682479ccec8b5d15e8870c311e1c844dbd172
Walter Simson <waltersimson@stanford.edu>
2023-02-09 16:07:51 -0800

    change project name

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 # from uff.version import __version__
 
 setup(
-    name='UFF.py',
-    version='0.3.0',
+    name='uff-reader',
+    version='0.0.1',
     description='Python interface for the Ultrasound File Format (UFF)',
     author='Walter Simson',
     author_email='walter.simson@tum.de',
